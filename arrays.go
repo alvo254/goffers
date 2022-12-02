b8920fd496ddf694bb1bddd21e8ee1f4e95bd84c
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-12-02 22:07:58 +0300

    arrays

---
+++ arrays.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Welcome to arrays")
+	//This elemnts are contigous in memory
+	//This is a literal sysntax this alloctes enough space to hold the array
+	grades := [...]int {87,34,90,45,23}
+	a := [...]int{1,2,3,4,5,6,7,8,9}
+	fmt.Println("a =",a[8])
+	var students [3]string
+	students[0] = "Lista"
+	students[1] = "Arnold"
+	fmt.Print(len(students))
+	fmt.Print(grades)
+	fmt.Print("students: %v\n",students[1])
+
+}
